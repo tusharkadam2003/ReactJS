fbe24fadf5c1d6a66e2d97893a0ca5197da40c99
Tushar Kadam <tusharkadam9151@gmail.com>
2024-07-27 23:55:03 +0530

    custom app (demo)

--- 01basicvitereact/src/main.jsx
+++ 01basicvitereact/src/main.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Index from './index.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Index from "./index.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<>
-    <App />
-    
-    <h1>Tushar Kadam</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-</>
-   
-  
-)
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<MyApp />);
