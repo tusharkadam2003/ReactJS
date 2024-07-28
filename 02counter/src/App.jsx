47b5d425aca3e1cd38b842ba07e0d56750206ea7
Tushar Kadam <tusharkadam9151@gmail.com>
2024-07-28 16:46:14 +0530

    make a clickable counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  let [counter, setCounter] = useState(0)
+  
+  const increaseValue = () => {
+    setCounter(counter + 1)
+  }
+
+  const decreaseValue = () => {
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+      <h1>Tushar Kadam</h1>
+      <h2>Counter Value {counter}</h2>
+      <button onClick={increaseValue}>Increase Value</button> <br />
+      <button onClick={decreaseValue}>Decrease Value</button>
+    </>
+  )
+}
+
+export default App
