d72584a0c4c054e6bead9df7fe2df2898ee20126
Tushar Kadam <tusharkadam9151@gmail.com>
2024-07-28 17:27:29 +0530

    add limit in counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -8,11 +8,18 @@ function App() {
   let [counter, setCounter] = useState(0)
   
   const increaseValue = () => {
-    setCounter(counter + 1)
+    if (counter < 20) {
+      setCounter(counter + 1)
+    }
+     
   }
 
   const decreaseValue = () => {
-    setCounter(counter - 1)
+    
+    if (counter > 0) {
+      setCounter(counter - 1)
+    }
+  
   }
 
   return (
