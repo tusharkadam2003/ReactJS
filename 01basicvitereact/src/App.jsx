0cf81ef3c39790526265a9a6ec45aaf655f22dcd
Tushar Kadam <tusharkadam9151@gmail.com>
2024-07-28 14:05:35 +0530

    add files to 01basicreact

--- 01basicvitereact/src/App.jsx
+++ 01basicvitereact/src/App.jsx
@@ -1,15 +1,11 @@
-
-
-
-
+import index from "./index.jsx"
 function App() {
- 
-
+  const username = 'tusharkadam'
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Hello World { username }</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 01basicvitereact/src/main.jsx
+++ 01basicvitereact/src/main.jsx
@@ -11,4 +11,29 @@ function MyApp() {
   );
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<MyApp />);
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click Me To Vist Google'
+// }
+
+const anotherElement = (
+    <a href="https://www.google.com" target="_blank">Visit google</a>
+)
+
+const anotherUser = ' react and javascript'
+
+const reactElement = React.createElement(
+    'a',
+    {
+        href: 'https://www.google.com ',target: '_blank'
+    },
+  'click to visit google ',
+    anotherElement
+)
+
+ReactDOM.createRoot(document.getElementById("root")).render( reactElement )  ;
